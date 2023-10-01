dd7d2381400a3afada8d73a9f7b7ddfa5728c288
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-10-01 03:37:37 +0100

    3.2 shows valid. and test error + best poly degree

--- lab2/tutorials3.2.py
+++ lab2/tutorials3.2.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -28,34 +29,50 @@ scaler = StandardScaler()
 # agesStd, lengthsStd = np.std(lines, axis=0)
 scaler.fit(trainingData)
 
-# Transform original training and validation data into their corresponding z-scores (based on the training data)
+# Transform the original data into their corresponding z-scores (based on the training mean)
 trainingDataZScores = scaler.transform(trainingData)
 validationDataZScores = scaler.transform(validationData)
+testDataZScores = scaler.transform(testData)
 
 # Separate ages and lengths columns into their own arrays
 trainingAgesZScore = getArrayColumn(trainingDataZScores, 0)
 trainingLengthsZScore = getArrayColumn(trainingDataZScores, 1)
 validationAgesZScore = getArrayColumn(validationDataZScores, 0)
 validationLengthsZScore = getArrayColumn(validationDataZScores, 1)
+testAgesZScore = getArrayColumn(testDataZScores, 0)
+testLengthsZScore = getArrayColumn(testDataZScores, 1)
 
-# Build graph with training and validation data points
+# Build graph with training, validation and test data points
 plt.figure("Blue gill size")
 plt.xlabel('Age')
 plt.ylabel('Length')
 plt.scatter(trainingAgesZScore, trainingLengthsZScore, s=5, c="b")
 plt.scatter(validationAgesZScore, validationLengthsZScore, s=1, c="r")
+plt.scatter(testAgesZScore, testLengthsZScore, s=1, c="g")
 
 # Create evenly spaced numbers on the x-axis (age) for smooth plotting
 xSeq = np.linspace(trainingAgesZScore.min(), trainingAgesZScore.max(), 1000).reshape(-1, 1)
 
+bestPolynomialDegree = 1
+lowestValidationError = math.inf
+
 # Create and plot polynomial (regression) curves from degrees 1 to 6, calculating their training and validation errors
 for polynomialDegree in range(1, 7) :
     coefficients = np.polyfit(trainingAgesZScore.flatten(), trainingLengthsZScore.flatten(), polynomialDegree)
+
     trainingError = mean_squared_error(trainingLengthsZScore, np.polyval(coefficients, trainingAgesZScore))
     validationError = mean_squared_error(validationLengthsZScore, np.polyval(coefficients, validationAgesZScore))
+
     plt.plot(xSeq, np.polyval(coefficients, xSeq), linewidth=0.75, label="{} / {} / {}".format(polynomialDegree, round(trainingError, 4), round(validationError, 4)))
+
+    if validationError < lowestValidationError :
+        lowestValidationError = validationError
+        bestPolynomialDegree = polynomialDegree
+
+print("Best polynomial degree for regression: {}".format(bestPolynomialDegree))
+print("Validation error: {}".format(lowestValidationError))
+testError = mean_squared_error(testLengthsZScore, np.polyval(coefficients, testAgesZScore))
+print("Test error: {}".format(testError))
 
 plt.legend(loc="lower right")
 plt.show()
-
-
