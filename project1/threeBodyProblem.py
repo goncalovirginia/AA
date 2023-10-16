cb391ff2af48bee299958b2f049d2bc5c81b5f65
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2023-10-16 21:15:27 +0100

    updated threeBodyProblem.py

--- project1/threeBodyProblem.py
+++ project1/threeBodyProblem.py
@@ -9,15 +9,21 @@ from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import QuantileTransformer
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import Lasso
 from sklearn.linear_model import LassoCV
 from sklearn.linear_model import MultiTaskLassoCV
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+import onnxruntime as rt
+from skl2onnx import to_onnx as sklearn_to_onnx
+from skl2onnx.common.data_types import FloatTensorType, StringTensorType
 
 LAST_ROW = 1285000
 NUM_SAMPLES = 5000
@@ -38,14 +44,19 @@ def plotAllSamples() :
     for startRow in range(0, LAST_ROW, SAMPLE_LENGTH) :
         plotSample(startRow)
 
-def create_X_train_formatted(X, y) :
-    #data_x2 = data_x.drop(columns=["Unnamed: 0", "Unnamed: 0.1"], inplace=True)
-    columns = y.drop('t', axis=1).columns
-    X['t'] = y['t']
-    X.drop(columns=["Unnamed: 0", "Unnamed: 0.1"], inplace=True)
-    X.to_csv("data_x.csv")
-    for j in range(1,257):
-        X.iloc[j] = y.iloc[0]
+def format_X_Train_for_input(X_train):
+    X1 = X_train.copy()
+    for i in range(0, NUM_SAMPLES):
+        for j in range(0, SAMPLE_LENGTH):
+            X1.iloc[[i*SAMPLE_LENGTH + j]] = X_train.iloc[[i*SAMPLE_LENGTH]]
+    X1['t'] = X_train['t']
+    return X1
+
+def addFeatureEngineering(df) :
+    df = addAccelerationsFeatures(df)
+    df = addPairDistancesFeatures(df)
+    df = addPairPotentialsFeatures(df)
+    return df
 
 # Predict with X_test.csv and generate the submission csv file
 def predictAndGenerateSubmissionCsv(filename) :
@@ -58,7 +69,7 @@ def predictAndGenerateSubmissionCsv(filename) :
 
     X_test_dataframe = addAccelerationsFeatures(X_test_dataframe)
     X_test_dataframe = addPairDistancesFeatures(X_test_dataframe)
-    X_test_dataframe = addInteractionPotentialsFeatures(X_test_dataframe)
+    X_test_dataframe = addPairPotentialsFeatures(X_test_dataframe)
     X_test_dataframe = X_test_dataframe[X.columns.tolist()]
 
     y_predicted = pipeline.predict(X_test_dataframe)
@@ -99,31 +110,62 @@ def addPairDistancesFeatures(X) :
 def safeDiv2(n, d) :
     return n / d if d else 0.0
 
-def rowInteractionPotentials(row) :
+def rowPairPotentials(row) :
     d_1_2, d_1_3, d_2_3 = rowPairDistances(row)
-    return [safeDiv2(1, d_1_2), safeDiv2(1, d_1_3), safeDiv2(1, d_2_3)]
+    return [safeDiv2(1, d_1_2**2), safeDiv2(1, d_1_3**2), safeDiv2(1, d_2_3**2)]
 
-def addInteractionPotentialsFeatures(X) :
-    X[['ip_1_2', 'ip_1_3', 'ip_2_3']] = X.apply(rowInteractionPotentials, axis=1, result_type='expand')
+def addPairPotentialsFeatures(X) :
+    X[['p_1_2', 'p_1_3', 'p_2_3']] = X.apply(rowPairPotentials, axis=1, result_type='expand')
     return X
 
-X = pd.read_csv("project1/X_train_acc_ip_pd.csv")
-y = pd.read_csv("project1/y_train_acc_ip_pd.csv")
+def toONNX(pipeline, name) :
+    initial_type = [('numfeat', FloatTensorType([None, 25]))]
+    model_onnx = sklearn_to_onnx(pipeline, initial_types=initial_type)
+
+    with open("model_acc_ip_pd_MAbsS_d3.onnx", "wb") as f:
+        f.write(model_onnx.SerializeToString())
+
+    sess = rt.InferenceSession("{}.onnx".format(name))#, providers=["CPUExecutionProvider"])
+
+    input_data = np.array(X_test, dtype=np.float32)
+    input_data = {'numfeat': input_data}
+    label_name = sess.get_outputs()[0].name
+    output = sess.run([label_name], input_data)
+    pred_onx = pd.DataFrame(output[0], columns=y.columns)
+
+    print("\nY:\n  {}".format(y_test))
+    print("\nskl_predict:\n  {}".format(y_predicted))
+    print("\nomnx_predict:\n {}".format(pred_onx))
+
+"""
+X_train = pd.read_csv("project1/X_train.csv").drop(columns=['Id'])
+X = format_X_Train_for_input(X_train)
+X = addFeatureEngineering(X)
+y = addFeatureEngineering(X_train)
+"""
+
+# Custom files used because FunctionTransformers take too long
+X = pd.read_csv("project1/X_train_a_d_p.csv")
+y = pd.read_csv("project1/y_train_a_d_p.csv")
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.2)
 
 print("Poly3 MaxAbsScaler Linear:")
 pipeline = make_pipeline(#FunctionTransformer(addAccelerationsFeatures), FunctionTransformer(addPairDistancesFeatures), FunctionTransformer(addInteractionPotentialsFeatures),
-    PolynomialFeatures(3), MaxAbsScaler(), LinearRegression(n_jobs=-1)) 
+    PolynomialFeatures(3), MaxAbsScaler(), LinearRegression()) 
 pipeline.fit(X_train, y_train)
 y_predicted = pd.DataFrame(pipeline.predict(X_test), columns=y.columns)
-print("RMSE: {}".format(math.sqrt(mean_squared_error(y_test[['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']], y_predicted[['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']]))))
+rmse = math.sqrt(mean_squared_error(y_test[['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']], y_predicted[['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']]))
+print("RMSE: {}".format(rmse))
 
-predictAndGenerateSubmissionCsv('poly3_maxabsscaler_linear')
+name = 'poly3_maxabs_linear_{}'.format(rmse)
+predictAndGenerateSubmissionCsv(name)
+toONNX(pipeline, name)
 
 """
 print("StandardScaler KNN Regression:")
 pipeline = make_pipeline(#FunctionTransformer(addAccelerationsFeatures), FunctionTransformer(addPairDistancesFeatures), FunctionTransformer(addInteractionPotentialsFeatures),
-    StandardScaler(), KNeighborsRegressor(n_jobs=-1)) 
+    StandardScaler(), KNeighborsRegressor()) 
 pipeline.fit(X_train, y_train)
 y_predicted = pd.DataFrame(pipeline.predict(X_test), columns=y.columns)
 print("RMSE: {}".format(math.sqrt(mean_squared_error(y_test[['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']], y_predicted[['x_1', 'y_1', 'x_2', 'y_2', 'x_3', 'y_3']]))))
